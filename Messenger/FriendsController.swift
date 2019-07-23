1d56b084de5ad27d391e383b14f35226f235c76c
christophwottawa <christoph.wottawa@me.com>
2019-07-23 20:50:36 +0200

    fetching data via CoreData, sorting latest messages by date

--- Messenger/FriendsController.swift
+++ Messenger/FriendsController.swift
@@ -67,19 +67,21 @@ extension FriendsController {
             mark.name = "Mark Zuckerberg"
             mark.profileImageName = "zuckprofile"
             
-            let message = NSEntityDescription.insertNewObject(forEntityName: "Message", into: context) as! Message
-            message.user = mark
-            message.text = "Hello, my name is Mark. Nice to meet you..."
-            message.date = Date()
+            createMessage(with: "Hi, I'm Mark! Nice to meet you...", user: mark, time: "08:21", context: context)
             
             let steve = NSEntityDescription.insertNewObject(forEntityName: "User", into: context) as! User
             steve.name = "Steve Jobs"
             steve.profileImageName = "steveprofile"
             
-            let steveMessage = NSEntityDescription.insertNewObject(forEntityName: "Message", into: context) as! Message
-            steveMessage.user = steve
-            steveMessage.text = "Hi, Steve Jobs here! Hope you like our latest iStuff"
-            steveMessage.date = Date()
+            createMessage(with: "Hi, Steve Jobs here! Hope you like our latest iStuff", user: steve, time: "12:03", context: context)
+            createMessage(with: "Hey, how are you, Bro?", user: steve, time: "14:10", context: context)
+            createMessage(with: "Come and check out the latest iPhone XII at your local Apple Store!", user: steve, time: "14:30", context: context)
+            
+            let donald = NSEntityDescription.insertNewObject(forEntityName: "User", into: context) as! User
+            donald.name = "Donald Trump"
+            donald.profileImageName = "donaldprofile"
+            
+            createMessage(with: "I'm the President!", user: donald, time: "18:20", context: context)
             
             do {
                 try context.save()
@@ -91,18 +93,62 @@ extension FriendsController {
         loadData()
     }
     
+    func createMessage(with text: String, user: User, time: String, context: NSManagedObjectContext) {
+        let message = NSEntityDescription.insertNewObject(forEntityName: "Message", into: context) as! Message
+        message.user = user
+        message.text = text
+        message.date = Date(timeString: time)
+    }
+    
     func loadData() {
+        //let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        
+        //get the users:
+        guard let users = fetchUsers() else { return }
+        messages = [Message]()
+        
+        //get their latest message
+        for user in users {
+            guard let set = user.messages as? Set<Message>, set.count > 0 else { continue }
+            let messages = set.sorted(by: { $0.date! < $1.date! })
+            self.messages?.append(messages.last!)
+        }
+        
+        messages?.sort(by: { $0.date! > $1.date! })
+        
+        /*
+        for user in users {
+            if let context = appDelegate?.persistentContainer.viewContext {
+                let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Message")
+                fetchRequest.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
+                fetchRequest.predicate = NSPredicate(format: "user.name = %@", user.name!)
+                fetchRequest.fetchLimit = 1
+                do {
+                    //messages?.append(contentsOf: try context.fetch(fetchRequest) as? [Message] ?? [])
+                    messages? += try context.fetch(fetchRequest) as? [Message] ?? []
+                } catch let error {
+                    print("Can not load data: \(error)")
+                }
+            }
+        }*/
+        
+    }
+    
+    fileprivate func fetchUsers() -> [User]? {
         let appDelegate = UIApplication.shared.delegate as? AppDelegate
         
         if let context = appDelegate?.persistentContainer.viewContext {
-            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Message")
+            
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
             
             do {
-                messages = try context.fetch(fetchRequest) as? [Message]
+                return try context.fetch(request) as? [User]
             } catch let error {
-                print("Can not load data: \(error)")
+                print("Could not load Users: \(error)")
             }
         }
+        
+        return nil
     }
     
     func clearData() {
@@ -135,5 +181,17 @@ extension UIView {
             withVisualFormat: format,
             metrics: nil,
             views: views))
+    }
+}
+
+extension Date {
+    init?(timeString: String) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        if let date = formatter.date(from: timeString) {
+            self = date
+        } else {
+            return nil
+        }
     }
 }
